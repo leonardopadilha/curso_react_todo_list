d7a2d25e9cf86a33a5ce27eac52318e2cebf3054
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-03-28 06:46:50 -0300

    Preparando o voltar para redirecionar o usuário para a tela principal

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,3 +1,4 @@
+import { ChevronLeftIcon } from "lucide-react";
 import { useSearchParams } from "react-router-dom";
 
 function TaskPage() {
@@ -6,8 +7,21 @@ function TaskPage() {
   const description = searchParams.get("description");
   return (
     <div className="h-screen w-screen bg-slate-500 p-6">
-      <h1>{title}</h1>
-      <p>{description}</p>
+      <div className="w-[500px] space-y-4">
+        <div className="flex justify-center relative mb-6">
+          <button className="absolute left-0 top-0 bottom-0 text-slate-100">
+            <ChevronLeftIcon />
+          </button>
+          <h1 className="text-3xl text-slate-100 font-bold text-center">
+            Detalhes da tarefa Tarefas
+          </h1>
+        </div>
+
+        <div className="bg-slate-200 p-4 rounded-md">
+          <h2 className="text-xl font-bold text-slate-600">{title}</h2>
+          <h2 className="text-slate-600">{description}</h2>
+        </div>
+      </div>
     </div>
   );
 }
